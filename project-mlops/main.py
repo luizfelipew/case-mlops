7b018a608ead2d7b75a8ddcb5a8d3ca69f7881c5
luizfelipew <luizfelipewendt@gmail.com>
2024-04-18 20:09:22 -0300

    serialização do modelo

--- project-mlops/main.py
+++ project-mlops/main.py
@@ -1,24 +1,27 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import pandas as pd
+import pickle
 
-df = pd.read_csv('casas.csv')
+# from sklearn.model_selection import train_test_split
+# import pandas as pd
+
+# df = pd.read_csv('casas.csv')
 colunas = ['tamanho', 'ano', 'garagem']
 
-# Variavel explicativa -> x
-X = df.drop('preco', axis=1)
-# Variavel resposta -> y
-y = df['preco']
+# # Variavel explicativa -> x
+# X = df.drop('preco', axis=1)
+# # Variavel resposta -> y
+# y = df['preco']
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.3, random_state=42)
 
-modelo = LinearRegression()
-# ajuste do modelo apartir da minha base de treinamento
-modelo.fit(X_train, y_train)
+# modelo = LinearRegression()
+# # ajuste do modelo apartir da minha base de treinamento
+# modelo.fit(X_train, y_train)
 
+modelo = pickle.load(open('modelo.sav', 'rb'))
 
 app = Flask(__name__)
 
